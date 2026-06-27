a17ee608a47840a4666db719910dfd4854887519
Flor <florazul.fb@gmail.com>
2026-06-27 13:27:28 +0200

    add items to the cart without duplicating them

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,51 @@
-import {useState} from 'react'
-import Guitar from "./components/Guitar"
-import Header from "./components/Header"
-import { db } from './data/db'
-
+import { useState } from "react";
+import Guitar from "./components/Guitar";
+import Header from "./components/Header";
+import { db } from "./data/db";
 
 function App() {
+  const [data, setData] = useState(db);
+  const [cart, setCart] = useState([]);
 
-    const [data, setData] = useState (db)
-    const [cart, setCart] = useState([]);
- 
+  function addToCart(item) {
+    const itemExist = cart.findIndex((guitar) => guitar.id === item.id);
+    if (itemExist >= 0) {
+      // existe en el carrito
+      console.log("Ya existe.");
+    } else {
+      item.quantity = 1;
+      setCart([...cart, item]);
+    }
+  }
+
   return (
     <>
-      
-     <Header/>
+      <Header />
 
-    <main className="container-xl mt-5">
+      <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-            {data.map( (guitar) =>  (
-                    <Guitar 
-                        key={guitar.id}
-                        guitar={guitar}
-                        setCart={setCart}
-                    />
-                )
-            )}
-            
+          {data.map((guitar) => (
+            <Guitar
+              key={guitar.id}
+              guitar={guitar}
+              setCart={setCart}
+              addToCart={addToCart}
+            />
+          ))}
         </div>
-    </main>
+      </main>
 
-
-    <footer className="bg-dark mt-5 py-5">
+      <footer className="bg-dark mt-5 py-5">
         <div className="container-xl">
-            <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+          <p className="text-white text-center fs-4 mt-4 m-md-0">
+            GuitarLA - Todos los derechos Reservados
+          </p>
         </div>
-    </footer>
-
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
